3da85ae58086bb8ee79e664951af7df2b9b1b082
itcuihao <it_cuihao@163.com>
2019-12-16 08:18:00 +0800

    update: 接口分离

--- design/principle/girl/girl.go
+++ design/principle/girl/girl.go
@@ -4,14 +4,30 @@ import "fmt"
 
 // 漂亮的女孩
 type IPettyGirl interface {
+	IGreatTemperamentGirl
+	IGoodBodyGirl
+
+	//接口分离原则
+	//greatTemperament()
+	//goodLooking()
+	//niceFigure()
+}
+
+// 内在美
+type IGreatTemperamentGirl interface {
+	greatTemperament()
+}
+
+// 外在美
+type IGoodBodyGirl interface {
 	goodLooking()
 	niceFigure()
-	greatTemperament()
 }
 
 // 实现星探
 type Searcher struct {
 	pettyGirl IPettyGirl
+	tempGirl IGreatTemperamentGirl
 }
 
 func (s *Searcher) AbstractSearcher(i IPettyGirl) {
@@ -25,6 +41,16 @@ func (s Searcher) show() {
 	s.pettyGirl.niceFigure()
 	s.pettyGirl.greatTemperament()
 }
+
+func (s *Searcher) abstractSearcherT(i IGreatTemperamentGirl) {
+	s.tempGirl = i
+}
+
+func (s Searcher) showt() {
+	fmt.Println("信息如下：")
+	s.pettyGirl.greatTemperament()
+}
+
 
 // 实现女孩
 type PettyGirl struct {
@@ -57,4 +83,10 @@ func run() {
 	searcher := &Searcher{}
 	searcher.AbstractSearcher(iGirl)
 	searcher.show()
+
+	var iTGirl IGreatTemperamentGirl
+	iTGirl = girl
+
+	searcher.abstractSearcherT(iTGirl)
+	searcher.showt()
 }
