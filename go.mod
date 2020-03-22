6f6741b138c845333df0849cb0744e970433e470
itcuihao <it_cuihao@163.com>
2020-03-22 23:04:23 +0800

    update

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.13
 
 require (
 	github.com/bndr/gotabulate v1.1.2
+	github.com/chromedp/cdproto v0.0.0-20200116234248-4da64dd111ac
 	github.com/chromedp/chromedp v0.5.3
+	github.com/go-sql-driver/mysql v1.4.1
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/jinzhu/gorm v1.9.12
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
--- images/screenshot/run.go
+++ images/screenshot/run.go
@@ -6,9 +6,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
+	"time"
 
+	"github.com/chromedp/cdproto/emulation"
+	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/device"
 )
 
 var (
@@ -62,4 +67,63 @@ func readHtml() string {
 	}
 	fmt.Println(string(data))
 	return string(data)
+}
+
+func ScreenShotURL(data string) ([]byte, error) {
+
+	ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithLogf(log.Printf))
+	defer cancel()
+
+	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+
+	var buf []byte
+	if err := chromedp.Run(ctx, fullScreenshot(data, 80, &buf)); err != nil {
+		// if err := chromedp.Run(ctx, captureScreenshot(data, &buf)); err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
+
+func fullScreenshot(urlstr string, quality int64, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Emulate(device.IPad),
+		chromedp.EmulateViewport(768, 1024, chromedp.EmulateScale(2)),
+		chromedp.Navigate(urlstr),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			// get layout metrics
+			_, _, contentSize, err := page.GetLayoutMetrics().Do(ctx)
+			if err != nil {
+				return err
+			}
+
+			width, height := int64(math.Ceil(contentSize.Width)), int64(math.Ceil(contentSize.Height))
+
+			// force viewport emulation
+			err = emulation.SetDeviceMetricsOverride(width, height, 1, false).
+				WithScreenOrientation(&emulation.ScreenOrientation{
+					Type:  emulation.OrientationTypePortraitPrimary,
+					Angle: 0,
+				}).
+				Do(ctx)
+			if err != nil {
+				return err
+			}
+			// capture screenshot
+			*res, err = page.CaptureScreenshot().
+				WithFormat(page.CaptureScreenshotFormatJpeg).
+				WithQuality(quality).
+				WithClip(&page.Viewport{
+					X:      contentSize.X,
+					Y:      contentSize.Y,
+					Width:  contentSize.Width,
+					Height: contentSize.Height,
+					Scale:  1,
+				}).Do(ctx)
+			if err != nil {
+				return err
+			}
+			return nil
+		}),
+	}
 }
